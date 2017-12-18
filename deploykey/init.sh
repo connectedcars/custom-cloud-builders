3c72e7509ebb5b683ad42599f4a6bda34116b60d
Troels Liebe Bentsen <tlb@nversion.dk>
2017-12-18 19:09:53 +0100

    Fix copy/paste typo

--- deploykey/init.sh
+++ deploykey/init.sh
@@ -10,8 +10,7 @@ fi
 PROJECT=$(echo $KMS_KEY_NAME | cut -f1 -d'/')
 LOCATION=$(echo $KMS_KEY_NAME | cut -f2 -d'/')
 KEYRING=$(echo $KMS_KEY_NAME | cut -f3 -d'/')
-KEYRING=$(echo $KMS_KEY_NAME | cut -f4 -d'/')
-KEY=$(echo $KMS_KEY_NAME | cut -f5 -d'/')
+KEY=$(echo $KMS_KEY_NAME | cut -f4 -d'/')
 
 echo gcloud --project $PROJECT kms decrypt --location=$LOCATION --keyring=$KEYRING --key=$KEY --ciphertext-file=/root/id_rsa.enc --plaintext-file=/root/id_rsa
 
